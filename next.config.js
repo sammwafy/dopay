d40166cc45d7fabdbe4ef4b0d3facd47fe78908a
Sameh Hassan <dent.semo@gmail.com>
2022-06-02 00:29:18 +0200

    basic layout

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const withAntdLess = require('next-plugin-antd-less');
 
 module.exports = withAntdLess({
   // optional: you can modify antd less variables directly here
-  modifyVars: { '@primary-color': '#04f' },
+  // modifyVars: { '@primary-color': '#04f' },
   // Or better still you can specify a path to a file 
   lessVarsFilePath: './styles/variables.less',
   // optional
@@ -19,13 +19,7 @@ module.exports = withAntdLess({
 
   // Other Config Here...
 
-  webpack(config) {
-    return config;
-  },
 
-  // ONLY for Next.js 10, if you use Next.js 11, delete this block
-  future: {
-    webpack5: true,
-  },
+
 });
 
