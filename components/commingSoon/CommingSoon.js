e81bf0f2e0b632ca96ffcd06dcd1f181df918e77
shaimaayosef <shimaa.sayed8888@gmail.com>
2022-06-03 01:21:42 +0200

    error404 & comming soon

--- components/commingSoon/CommingSoon.js
+++ components/commingSoon/CommingSoon.js
@@ -3,7 +3,10 @@ import { Comming } from "./styles/CommingSoon.styled";
 const CommingSoon = () => {
   return (
     <Comming>
-      <div>comming soon</div>
+      <div className="comming-soon">
+        <h1>Coming Soon</h1>
+        <h2>This Section will be ready soon</h2>
+      </div>
     </Comming>
   );
 };
--- components/commingSoon/styles/CommingSoon.styled.js
+++ components/commingSoon/styles/CommingSoon.styled.js
@@ -2,7 +2,35 @@ import styled from "styled-components";
 
 export const Comming = styled.div`
   border: 3px rebeccapurple solid;
-  height: 80px;
-  width: 40px;
-  color: red;
+  .comming-soon {
+    display: flex;
+    flex-direction: column;
+    width: 800px;
+    margin: 0 auto;
+    align-items: center;
+    @media screen and (max-width: 700px) {
+      width: auto;
+    }
+    h1 {
+      font-style: normal;
+      font-weight: 700;
+      font-size: 72px;
+      line-height: 88px;
+      text-align: center;
+      color: rgba(0, 0, 0, 0.87);
+      @media screen and (max-width: 700px) {
+        font-weight: 500;
+        font-size: 52px;
+      }
+    }
+    h2 {
+      font-style: normal;
+      font-weight: 500;
+      font-size: 36px;
+      line-height: 44px;
+      text-align: center;
+      text-transform: capitalize;
+      color: rgba(0, 0, 0, 0.54);
+    }
+  }
 `;
