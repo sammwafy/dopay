fd0eb50202d4c2aa1c5a558cd9e09284f81bbf95
Ahmed Al Qady <26540442+ABQady@users.noreply.github.com>
2022-06-12 00:43:27 +0200

    Managing Packages

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
       layoutRaw: true,
     },
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // set 'fs' to an empty module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config;
+  }
 };
 
 module.exports = nextConfig;
