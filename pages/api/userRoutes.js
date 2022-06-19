9a10501fe3318ae509c6194bc8dc5a5e0d481a43
Ahmed Al Qady <26540442+ABQady@users.noreply.github.com>
2022-06-19 22:25:19 +0200

    Need more work

--- pages/api/userRoutes.js
+++ pages/api/userRoutes.js
@@ -1,6 +1,6 @@
 //const { getUserController, createUserController, updateNotifications } = require("../api/userService");
 const { dbConnect } = require("../../db/middleware/mongodb");
-const { nanoid } = require("nanoid");
+//const { nanoid } = require("nanoid");
 const UserModel = require("../../models/userModel");
 const sse = require("../api/sse");
 
@@ -9,7 +9,7 @@ const adminID = '1234';
 const createNotification = ({ accID, userID }) => {
    console.log("notif created");
    return {
-      id: nanoid(),
+      id: userID,
       userID: userID,
       title: `New account ${accID} created by user ${userID}`,
       type: "user",
@@ -25,46 +25,39 @@ const updateNotifications = (accID, userID) => {
    const _notif = createNotification({ accID, userID });
    const data = { liker: userID };
    // emit post_reaction event to all users
-   sse.send(data, "post_reaction");
+   sse.send(data, "new_account");
    // emit notification_{userId} to the admin
-   //sse.send(_notif, `notification-${adminID}`);
+   sse.send(_notif, `notification-${adminID}`);
    console.log("Sent");
    if (userID === adminID) {
 
    }
 };
 
-export default async function userCheck(req, res) {
-   await dbConnect().then(console.log("connected"));
+// export default async function userCheck(req, res) {
+//    await dbConnect().then(console.log("connected"));
 
-   if (req.method === "POST") {
-      //const cookies = req.cookies;
-      console.log("testing POST");
+//    if (req.method === "POST") {
+//       //const cookies = req.cookies;
+//       console.log("testing POST");
 
-      const { accID, userID } = req.body;
+//       const { accID, userID } = req.body;
 
-      if (accID && userID) {
-         console.log("accID:");
-         console.log(accID);
-         console.log("UserId:");
-         console.log(userID);
+//       if (accID && userID) {
+//          console.log("accID:");
+//          console.log(accID);
+//          console.log("UserId:");
+//          console.log(userID);
 
-         updateNotifications({ accID, userID });
-         res.json("");
-      }
-      //res.json({});
-   } else if (req.method === "GET") {
-      res.json({});
-   } else {
-      res.status(422).send("req_method_not_supported");
-   }
+//          updateNotifications({ accID, userID });
+//          res.json("");
+//       }
+//       //res.json({});
+//    } else if (req.method === "GET") {
+//       res.json({});
+//    } else {
+//       res.status(422).send("req_method_not_supported");
+//    }
+// };
 
-};
-
-// router.get("/post", getUserController);
-
-// router.post("/user", createUserController);
-
-// router.patch("/user/:id", updateNotifications);
-
-// module.exports = router;
+module.exports = { updateNotifications }
