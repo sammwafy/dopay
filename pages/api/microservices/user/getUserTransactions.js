db269bebccfede0cb86dbc7b88baadfb71fb7fef
Sameh Hassan <dent.semo@gmail.com>
2022-06-24 01:13:47 +0200

    updates

--- pages/api/microservices/user/getUserTransactions.js
+++ pages/api/microservices/user/getUserTransactions.js
@@ -1,7 +1,4 @@
-import { Transactions } from "../../../db/models/transactions";
 import { Accounts } from "../../../db/models/accounts";
-import { notification } from "antd";
-import { mongo } from "mongoose";
 const { dbConnect } = require("../../../db/middleware/mongodb");
 export default async function getUserTransactions(req, res) {
   await dbConnect().then(console.log("connected"));
