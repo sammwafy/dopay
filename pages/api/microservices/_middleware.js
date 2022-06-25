c70170843462ac39be2aff76648aab43f357b203
Sameh Hassan <dent.semo@gmail.com>
2022-06-26 00:11:53 +0200

    make it better

---
+++ pages/api/microservices/_middleware.js
@@ -0,0 +1,66 @@
+import * as jose from "jose";
+
+import { NextRequest } from "next/server";
+
+export async function middleware(NextRequest) {
+  if (NextRequest.nextUrl.pathname.startsWith("/user")) {
+    const authorization = req.headers.get("authorization");
+    const userID = req.headers.get("userID");
+
+    if (authorization) {
+      const token = authorization.split(" ")[1];
+      try {
+        const { payload: jwtData } = await jose.jwtVerify(
+          token,
+          new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET)
+        );
+
+        NextResponse.next();
+      } catch (error) {
+        return new Response(JSON.stringify({ error: error.message }), {
+          status: 401,
+        });
+      }
+    } else {
+      return new Response(
+        JSON.stringify({ error: "you have to login first !" }),
+        {
+          status: 401,
+        }
+      );
+    }
+  }
+
+  if (NextRequest.nextUrl.pathname.startsWith("/admin")) {
+    const authorization = req.headers.get("authorization");
+
+    if (authorization) {
+      const token = authorization.split(" ")[1];
+      try {
+        const { payload: jwtData } = await jose.jwtVerify(
+          token,
+          new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET)
+        );
+        console.log(jwtData);
+        if (jwtData?.UserInfo?.isAdmin) {
+          NextResponse.next();
+        } else {
+          throw new Error("you need to be admin to access this");
+        }
+      } catch (error) {
+        console.log("from catch: ", error);
+
+        return new Response(JSON.stringify({ error: error.message }), {
+          status: 401,
+        });
+      }
+    } else {
+      return new Response(
+        JSON.stringify({ error: "you have to login first !" }),
+        {
+          status: 401,
+        }
+      );
+    }
+  }
+}
